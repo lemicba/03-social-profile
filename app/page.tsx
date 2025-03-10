d5698959f21af811e59f35899ae1bf5ccae9e6aa
lemicba <lemicba@gmail.com>
2025-03-10 16:46:18 -0300

    Add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import imageAvatar from '@/public/images/avatar-jessica.jpeg';
 import CustomButton from "./components/customButton";
 
 export default function Home() {
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="w-full min-h-screen flex flex-col justify-center overflow-x-hidden bg-gray-900">
       <main className="flex justify-center">
         <article className="bg-gray-800 w-[327px] sm:w-[384px] h-[579px] sm:h-[611px] rounded-xl p-10 flex-center flex-col gap-y-6">
-          <Image src="/images/avatar-jessica.jpeg" width={88} height={88} alt="Avatar image" className="rounded-full" />
+          <Image src={imageAvatar} width={88} height={88} alt="Avatar image" className="rounded-full" />
           <div>
             <h1 className="title text-white">Jessica Randall</h1>
             <p className="base-bold text-green-900">London, United Kingdom</p>
